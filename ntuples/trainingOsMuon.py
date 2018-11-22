e74622fc3fc70cbd8cf8993978081874c1e7bb80
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2018-11-22 18:02:43 +0100

    baseline training file

--- ntuples/trainingOsMuon.py
+++ ntuples/trainingOsMuon.py
@@ -43,7 +43,6 @@ def getKerasModel(inputDim, modelName, layerSize = 100, nLayers = 5, dropValue =
     return modelName
 
 
-
 # Setup TMVA
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
@@ -71,9 +70,6 @@ cut += '&&!TMath::IsNaN(muoDxy)'
 cutSgn = cut + '&&osMuonTag==1'
 cutBkg = cut + '&&osMuonTag==0'
 
-treeSgn = tree.CopyTree(cutSgn)
-treeBkg = tree.CopyTree(cutBkg)
-
 # Prepare factory
 
 name = 'OsMuon' + year
@@ -88,58 +84,33 @@ factory = TMVA.Factory('TMVAClassification', output,
 dataloader = TMVA.DataLoader('dataset')
 
 varList = [
-    ('muoPt', 'F', True)
-    ,('muoEta', 'F', True)
-    ,('muoDxy', 'F', True)
-    ,('muoDz', 'F', True)
-    ,('muoSoftMvaValue', 'F', True)
-    ,('muoDR_B', 'F', True)
-    ,('muoPFIso', 'F', True)
-    ,('muoJetPt', 'F', False)
-    ,('muoPtRel', 'F', False)
-    ,('muoDrJet', 'F', False)
-    ,('muoEnergyRatio', 'F', False)
-    ,('muoJetCSV', 'F', False)
-    ,('muoJetDFprob', 'F', False)
-    ,('muoJetSize', 'I', False)
-    ,('muoQCone', 'F', False)
+    ('muoPt', 'F')
+    ,('muoEta', 'F')
+    ,('muoDxy', 'F')
+    ,('muoDz', 'F')
+    ,('muoSoftMvaValue', 'F')
+    ,('muoDR_B', 'F')
+    ,('muoPFIso', 'F')
+    ,('muoJetPt', 'F')
+    ,('muoPtRel', 'F')
+    ,('muoDrJet', 'F')
+    ,('muoEnergyRatio', 'F')
+    ,('muoJetCSV', 'F')
+    ,('muoJetDFprob', 'F')
+    ,('muoJetSize', 'I')
+    ,('muoQCone', 'F')
     ]
-
-nVars = 0
-nVarsJet = 0
-nVarsNoJet = 0
-JetVars = ''
-noJetVars = ''
 
 for var in varList:
     if year == '2016' and var[0] == 'muoJetDFprob':
         continue
     dataloader.AddVariable( var[0], var[1] )
-    JetVars += var[0] + ':'
     nVars += 1
-    nVarsJet += 1
-    if var[2] == True:
-        noJetVars += var[0] + ':'
-        nVarsNoJet += 1
-
-JetVars = JetVars[:-1]
-noJetVars= noJetVars[:-1]
 
 # prepare dataloader
 
-if FIXFLAG:
-    dataloader.AddSignalTree(treeSgnTrain, 1.0, TMVA.Types.kTraining)
-    dataloader.AddBackgroundTree(treeBkgTrain, 1.0, TMVA.Types.kTraining)
-
-    dataloader.AddSignalTree(treeSgnTest, 1.0, TMVA.Types.kTesting)
-    dataloader.AddBackgroundTree(treeBkgTest, 1.0, TMVA.Types.kTesting)
-
-    cutSgn = ''
-    cutBkg = ''
-
-else:
-    dataloader.AddSignalTree(tree, 1.0)
-    dataloader.AddBackgroundTree(tree, 1.0)
+dataloader.AddSignalTree(tree, 1.0)
+dataloader.AddBackgroundTree(tree, 1.0)
 
 dataloaderOpt = 'SplitMode=Random:NormMode=NumEvents:V'
 
@@ -148,59 +119,36 @@ dataloader.PrepareTrainingAndTestTree(TCut(cutSgn), TCut(cutBkg), dataloaderOpt)
 # Define Keras Model
 if DNNFLAG:
     modelName = getKerasModel(nVars, 'model' + year + '.h5')
-    modelNameJet = getKerasModel(nVarsJet, 'modelJet' + year + '.h5')
-    modelNameNoJet = getKerasModel(nVarsNoJet, 'modelnoJet' + year + '.h5')
 
     # Book methods
     dnnOptions = '!H:!V:NumEpochs=1000:TriesEarlyStopping=50:BatchSize=32:FilenameModel='
 
-    dnnOptionsJet = dnnOptions + modelNameJet
-    dnnOptionsNoJet = dnnOptions + modelNameNoJet
     dnnOptions = dnnOptions + modelName
 
-    transOptJet = ':VarTransform=N'
-    transOptJet += ',G('
-    transOptnoJet = transOptJet
+    preprocessingOptions = ':VarTransform=N'
+    preprocessingOptions += ',G('
 
     for var in varList:
         if year == '2016' and var[0] == 'muoJetDFprob':
             continue
         if var[1] == 'F':
-            transOptJet += var[0] + ','
-            if var[2] == True:
-                transOptnoJet += var[0] + ','
+            preprocessingOptions += var[0] + ','
 
-    transOptJet = transOptJet[:-1]
-    transOptJet +=  '),N'
-
-    transOptnoJet = transOptnoJet[:-1]
-    transOptnoJet +=  '),N'
+    preprocessingOptions = preprocessingOptions[:-1]
+    preprocessingOptions +=  '),N'
 
     dnnName = 'DNN' + name
 
-    factory.BookMethod(dataloader, TMVA.Types.kPyKeras, dnnName, dnnOptions)
-
-    if CATFLAG:
-        dnnCat = factory.BookMethod(dataloader, TMVA.Types.kCategory, dnnName + 'Category', '')
-        dnnCat.AddMethod(TCut('muoJetSize>=0'), JetVars, TMVA.Types.kPyKeras, dnnName + 'Jet', dnnOptionsJet + transOptJet)
-        dnnCat.AddMethod(TCut('muoJetSize<0'), noJetVars, TMVA.Types.kPyKeras, dnnName + 'noJet', dnnOptionsNoJet +  transOptnoJet)
+    factory.BookMethod(dataloader, TMVA.Types.kPyKeras, dnnName, dnnOptions + preprocessingOptions)
 
 # BDT
 if BDTFLAG:
-    bdtOptions = '!H:!V:UseBaggedBoost:BaggedSampleFraction=0.2:NTrees=600:MaxDepth=6:nCuts=50:MinNodeSize=1.51%:BoostType=RealAdaBoost:AdaBoostBeta=0.3:DoBoostMonitor=True'
+    bdtOptions = '!H:!V:UseBaggedBoost:BaggedSampleFraction=0.2:NTrees=600:MaxDepth=6:nCuts=50:MinNodeSize=1.5%:BoostType=RealAdaBoost:AdaBoostBeta=0.3:DoBoostMonitor=True'
     bdtName = 'BDT' + name
     factory.BookMethod(dataloader, TMVA.Types.kBDT, bdtName, bdtOptions)
-
-    if CATFLAG:
-        bdtCat = factory.BookMethod(dataloader, TMVA.Types.kCategory, bdtName + 'Category', '')
-        bdtCat.AddMethod(TCut('muoJetSize>=0'), JetVars, TMVA.Types.kBDT, bdtName + 'Jet', bdtOptions)
-        bdtCat.AddMethod(TCut('muoJetSize<0'), noJetVars, TMVA.Types.kBDT, bdtName + 'noJet', bdtOptions)
 
 
 # Run training, test and evaluation
 factory.TrainAllMethods()
 factory.TestAllMethods()
 factory.EvaluateAllMethods()
-
-if FIXFLAG:
-    os.remove("./temp.root")
