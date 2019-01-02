cfd5cf5bed9858278e65e8c9c1bad3a6906d35fb
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-01-02 20:20:53 +0100

    Input stream prototype

--- ntuples/MVAtraining/fitMVA.C
+++ ntuples/MVAtraining/fitMVA.C
@@ -15,14 +15,55 @@ void setGvars(TString filename)
     dir_ = dirPath;
 }
 
-void fitMVA(TString file = "../BsMC/ntuBsMC2017.root", TString method = "BDTOsMuon2017test999")
+void fitMVA(TString file = "../BsMC/ntuBsMC2017.root"
+            , TString method = "BDTOsMuon2017test999"
+            , TString mode = "CREATE")
 {
     gErrorIgnoreLevel = kWarning;
+    if(mode != "CREATE" && mode != "CAT" && mode != "FUNCTION"){
+        cout<<"WRONG MODE"<<endl;
+        return false;
+    }
+
     TFile *f = new TFile(file);
     TTree *t = (TTree*)f ->Get("PDsecondTree");
 
     setGvars(file);
 
+    float *catEdgeL;
+    float *catEdgeR;
+    float *catMistag;
+
+//----------READ CATEGORIES FILE----------
+    if(mode == "CAT")
+    {
+        std::ifstream ifs("cat.txt",std::ifstream::in);
+        if(!ifs.is_open()) return;
+
+        int nCat;
+        ifs >> nCat;
+
+        catEdgeL = new float[nCat];
+        catEdgeR = new float[nCat];
+        catMistag = new float[nCat];
+
+        for(int i=0; i<nCat; ++i){
+            ifs >> catEdgeL[i];
+            ifs >> catEdgeR[i];
+            ifs >> catMistag[i];
+
+        }
+
+        ifs.close();
+    }
+
+//----------READ FUNCTION FILE----------
+    if(mode == "FUNCTION")
+    {
+        cout<<"WIP"<<endl;
+    }
+    
+//----------COMPUTE MVA----------
     TMVA::Reader reader("!Color:Silent");
     TMVA::PyMethodBase::PyInitialize();
 
@@ -144,9 +185,6 @@ void fitMVA(TString file = "../BsMC/ntuBsMC2017.root", TString method = "BDTOsMu
         }
     }
 
-    cout<<"----- MVA HISTOGRAMS FILLED"<<endl;
-
-    //----------KDE----------
     int nRT = vKDERT.size();
     int nWT = vKDEWT.size();
 
@@ -158,213 +196,225 @@ void fitMVA(TString file = "../BsMC/ntuBsMC2017.root", TString method = "BDTOsMu
     float xMin = *itMinWT < *itMinRT ? *itMinWT : *itMinRT;
     float xMax = *itMaxRT > *itMaxWT ? *itMaxRT : *itMaxWT;
 
-    float rhoRT = 0.1; // = pow(nRT,-1./5);
-    float rhoWT = 0.1; // = pow(nWT,-1./5);
 
-    mva_RT->GetXaxis()->SetRangeUser(xMin, xMax);
-    mva_WT->GetXaxis()->SetRangeUser(xMin, xMax);
+    cout<<"----- MVA HISTOGRAMS FILLED"<<endl;
 
-    cout<<"nRT "<<nRT<<endl;
-    cout<<"nWT "<<nWT<<endl;
-    cout<<"xMin "<<xMin<<endl;
-    cout<<"xMax "<<xMax<<endl;    
-    cout<<"rhoRT "<<rhoRT<<endl;
-    cout<<"rhoWT "<<rhoWT<<endl;
-
-    TKDE::EMirror mirror = TKDE::kMirrorBoth;
-
-    TKDE *kdeRT = new TKDE(nRT, &vKDERT[0],&vKDERT_w[0],xMin,xMax,
-        "KernelType:Epanechnikov;Iteration:Adaptive;Mirror:noMirror;Binning:RelaxedBinning", rhoRT);
-    TKDE *kdeWT = new TKDE(nWT, &vKDEWT[0],&vKDEWT_w[0],xMin,xMax,
-        "KernelType:Epanechnikov;Iteration:Adaptive;Mirror:noMirror;Binning:RelaxedBinning", rhoWT);
-
-    TF1 *pdfRT = new TF1("pdfRT",kdeRT,xMin,xMax,0);
-    TF1 *pdfWT = new TF1("pdfWT",kdeWT,xMin,xMax,0);
-
-    pdfRT->SetLineColor(kBlue);
-    pdfRT->SetNpx(2000);
-    pdfWT->SetLineColor(kRed);
-    pdfWT->SetNpx(2000);
-
-/*    auto fRT = new TF1("fRT",
-        [&](double *x, double *p) { 
-            return mva_RT->Integral()*(*kdeRT)(x[0]); }, 
-        xMin,xMax,0);
-
-    auto fWT = new TF1("fRT",
-        [&](double *x, double *p) { 
-            return mva_WT->Integral()*(*kdeWT)(x[0]); }, 
-        xMin,xMax,0);
-*/
-
-    TCanvas *c10 = new TCanvas();
-    c10->Divide(2,2);
-    c10->cd(1);
-    mva_RT->Draw("hist");
-    c10->cd(2);
-    mva_WT->Draw("hist");
-    c10->cd(3);
-    pdfRT->Draw();
-    c10->cd(4);
-    pdfWT->Draw();
-
-    auto fW = new TF1("fW",
-        [&](double *x, double *p) { 
-            double yRT = nRT*(*kdeRT)(x[0]); 
-            double yWT = nWT*(*kdeWT)(x[0]); return yWT/(yWT+yRT); }, 
-        xMin,xMax,0);
-
-    TCanvas *c12 = new TCanvas();
-    fW->SetMarkerStyle(20);
-    fW->SetMarkerSize(1.);
-    fW->SetNpx(25);
-    fW->DrawClone("P");
-
-    //----------CATEGORIES----------
-/*
-    TString base =  "evtWeight*((";
-    TString cutRT = base + ")&&osMuon&&osMuonTag==1)";
-    TString cutWT = base + ")&&osMuon&&osMuonTag==0)";
-
-    t->Project("mva", "osMuonTagMvaValue", cutRT + "||" + cutWT);
-    t->Project("mva_RT", "osMuonTagMvaValue", cutRT);
-    t->Project("mva_WT", "osMuonTagMvaValue", cutWT);
-*/
-
-/*
-    TH1F *mB       = new TH1F( "mB", "mB", nBins_, min_, max_ );
-    TH1F *mB_RT    = new TH1F( "mB_RT", "mB_RT", nBins_, min_, max_ );
-    TH1F *mB_WT    = new TH1F( "mB_WT", "mB_WT", nBins_, min_, max_ );
-    t->Project("mB", "mBMass", base + "))");
-    t->Project("mB_RT", "mBMass", cutRT );
-    t->Project("mB_WT", "mBMass", cutWT );
-*/
-
-
-    int nCat = 25;
-    int nTotTagged = nRT + nWT;
-    int catSize = nTotTagged / nCat;
-    cout<<endl<<nTotTagged<<endl<<catSize<<endl<<endl;
-
-    int cTot = 0;
-    int hTot = 0;
-    int cCenter = 0;
-    int lastCat = 0;
-    
-    float   *catEdge    = new float[nCat];
-    float   *catCenter  = new float[nCat];
-    int     *catRT      = new int[nCat];
-    int     *catWT      = new int[nCat];
-    float   *catEff     = new float[nCat];
-    float   *catW       = new float[nCat];
-    float   *catP       = new float[nCat];
-
-    float   *vexl = new float[nCat];
-    float   *vexh = new float[nCat];
-    float   *vey = new float[nCat];
-
-    for(int i=0; i<nCat; ++i){
-        catRT[i] = 0;
-        catWT[i] = 0;
-        catCenter[i] = 0;
-    }
-
-    int cCat = 0;
-    for(int i = 1; i<=nBinsMva; ++i){
-        cCenter += mva->GetBinCenter(i)*mva->GetBinContent(i);
-        catRT[cCat] += mva_RT->GetBinContent(i);
-        catWT[cCat] += mva_WT->GetBinContent(i);
-        cTot = cTot + mva_RT->GetBinContent(i) + mva_WT->GetBinContent(i);
-        hTot = hTot  + mva->GetBinContent(i);
-        //cout<<"bin "<<i<<", "<<mva_RT->GetBinContent(i) + mva_WT->GetBinContent(i);
-        //cout<<" ("<<cTot<<")"<<" ["<<hTot<<"]"<<endl;
-        if(cTot >= catSize){
-            catEdge[cCat] = mva->GetXaxis()->GetBinLowEdge(i+1);
-            catCenter[cCat] = (float)cCenter/(float)cTot;
-            cout<<" -----> cat "<<cCat<<", "<<cTot<<" ["<<catEdge[cCat]<<"]"<<endl;
-            cTot = 0;
-            cCenter = 0;
-            cCat++;
-        }
-        if(i==nBinsMva){
-            catCenter[cCat] = (float)cCenter/(float)cTot;
-            catEdge[cCat] = xMax;
-            lastCat = cCat;
-            cout<<" -----> cat "<<cCat<<", "<<cTot<<" ["<<catEdge[cCat]<<"]"<<endl;
-        }
-    }
-
-    cout<<endl;
-    nCat = lastCat + 1;
-    float totEff = 0;
-    float totW = 0;
-    float avgD = 0;
-    float totP = 0;
-
-    for(int i=0; i<nCat; ++i)
+//----------CREATE CATEGORIES----------
+    if(mode == "CREATE")
     {
-        catEff[i] = (float)(catWT[i] + catRT[i]) / (float)nEvents;
-        totEff += catEff[i];
 
-        catW[i] = (float)catWT[i] / (float)(catWT[i] + catRT[i]);
-        avgD += catEff[i]*(1-2*catW[i]);
+        //----------KDE----------
+        float rhoRT = 0.1; // = pow(nRT,-1./5);
+        float rhoWT = 0.1; // = pow(nWT,-1./5);
 
-        catP[i] = catEff[i]*pow(1-2*catW[i],2);
-        totP += catP[i];
+        mva_RT->GetXaxis()->SetRangeUser(xMin, xMax);
+        mva_WT->GetXaxis()->SetRangeUser(xMin, xMax);
+
+        cout<<"nRT "<<nRT<<endl;
+        cout<<"nWT "<<nWT<<endl;
+        cout<<"xMin "<<xMin<<endl;
+        cout<<"xMax "<<xMax<<endl;    
+        cout<<"rhoRT "<<rhoRT<<endl;
+        cout<<"rhoWT "<<rhoWT<<endl;
+
+        TKDE::EMirror mirror = TKDE::kMirrorBoth;
+
+        TKDE *kdeRT = new TKDE(nRT, &vKDERT[0],&vKDERT_w[0],xMin,xMax,
+            "KernelType:Epanechnikov;Iteration:Adaptive;Mirror:noMirror;Binning:RelaxedBinning", rhoRT);
+        TKDE *kdeWT = new TKDE(nWT, &vKDEWT[0],&vKDEWT_w[0],xMin,xMax,
+            "KernelType:Epanechnikov;Iteration:Adaptive;Mirror:noMirror;Binning:RelaxedBinning", rhoWT);
+
+        TF1 *pdfRT = new TF1("pdfRT",kdeRT,xMin,xMax,0);
+        TF1 *pdfWT = new TF1("pdfWT",kdeWT,xMin,xMax,0);
+
+        pdfRT->SetLineColor(kBlue);
+        pdfRT->SetNpx(2000);
+        pdfWT->SetLineColor(kRed);
+        pdfWT->SetNpx(2000);
+
+    /*    auto fRT = new TF1("fRT",
+            [&](double *x, double *p) { 
+                return mva_RT->Integral()*(*kdeRT)(x[0]); }, 
+            xMin,xMax,0);
+
+        auto fWT = new TF1("fRT",
+            [&](double *x, double *p) { 
+                return mva_WT->Integral()*(*kdeWT)(x[0]); }, 
+            xMin,xMax,0);
+    */
+
+        TCanvas *c10 = new TCanvas();
+        c10->Divide(2,2);
+        c10->cd(1);
+        mva_RT->Draw("hist");
+        c10->cd(2);
+        mva_WT->Draw("hist");
+        c10->cd(3);
+        pdfRT->Draw();
+        c10->cd(4);
+        pdfWT->Draw();
+
+        auto fW = new TF1("fW",
+            [&](double *x, double *p) { 
+                double yRT = nRT*(*kdeRT)(x[0]); 
+                double yWT = nWT*(*kdeWT)(x[0]); return yWT/(yWT+yRT); }, 
+            xMin,xMax,0);
+
+        TCanvas *c12 = new TCanvas();
+        fW->SetMarkerStyle(20);
+        fW->SetMarkerSize(1.);
+        fW->SetNpx(25);
+        fW->DrawClone("P");
+
+        //----------CATEGORIES----------
+    /*
+        TString base =  "evtWeight*((";
+        TString cutRT = base + ")&&osMuon&&osMuonTag==1)";
+        TString cutWT = base + ")&&osMuon&&osMuonTag==0)";
+
+        t->Project("mva", "osMuonTagMvaValue", cutRT + "||" + cutWT);
+        t->Project("mva_RT", "osMuonTagMvaValue", cutRT);
+        t->Project("mva_WT", "osMuonTagMvaValue", cutWT);
+    */
+
+    /*
+        TH1F *mB       = new TH1F( "mB", "mB", nBins_, min_, max_ );
+        TH1F *mB_RT    = new TH1F( "mB_RT", "mB_RT", nBins_, min_, max_ );
+        TH1F *mB_WT    = new TH1F( "mB_WT", "mB_WT", nBins_, min_, max_ );
+        t->Project("mB", "mBMass", base + "))");
+        t->Project("mB_RT", "mBMass", cutRT );
+        t->Project("mB_WT", "mBMass", cutWT );
+    */
 
 
-        vexh[i] = catEdge[i] - catCenter[i];
-        if(i!=0) vexl[i] = catCenter[i] - catEdge[i-1];
-        else     vexl[i] = catCenter[i] - xMin;
+        int nCat = 25;
+        int nTotTagged = nRT + nWT;
+        int catSize = nTotTagged / nCat;
+        cout<<endl<<nTotTagged<<endl<<catSize<<endl<<endl;
 
-        vey[i] = sqrt(((float)catWT[i] * (float)catRT[i])/pow(((float)catWT[i] + (float)catRT[i]),3) );
+        int cTot = 0;
+        int hTot = 0;
+        int cCenter = 0;
+        int lastCat = 0;
+        
+        float   *catEdge    = new float[nCat];
+        float   *catCenter  = new float[nCat];
+        int     *catRT      = new int[nCat];
+        int     *catWT      = new int[nCat];
+        float   *catEff     = new float[nCat];
+        float   *catW       = new float[nCat];
+        float   *catP       = new float[nCat];
 
-        cout<<"CAT "<<i+1;
-        if(i!=0) cout<<" ["<<catEdge[i-1]<<" - "<<catEdge[i]<<" ] ";
-        else     cout<<" ["<<xMin<<" - "<<catEdge[i]<<" ] ";
-        cout<<" - eff = "<<100*catEff[i]<<"%, w = "<<100*catW[i];
-        cout<<"%, P = "<<100*catP[i]<<"%"<<endl;
-        //cout<<" - "<<catCenter[i]<<" - ";
-        //cout<<catRT[i] + catWT[i]<<endl;
+        float   *vexl = new float[nCat];
+        float   *vexh = new float[nCat];
+        float   *vey = new float[nCat];
+
+        for(int i=0; i<nCat; ++i){
+            catRT[i] = 0;
+            catWT[i] = 0;
+            catCenter[i] = 0;
+        }
+
+        int cCat = 0;
+        for(int i = 1; i<=nBinsMva; ++i){
+            cCenter += mva->GetBinCenter(i)*mva->GetBinContent(i);
+            catRT[cCat] += mva_RT->GetBinContent(i);
+            catWT[cCat] += mva_WT->GetBinContent(i);
+            cTot = cTot + mva_RT->GetBinContent(i) + mva_WT->GetBinContent(i);
+            hTot = hTot  + mva->GetBinContent(i);
+            //cout<<"bin "<<i<<", "<<mva_RT->GetBinContent(i) + mva_WT->GetBinContent(i);
+            //cout<<" ("<<cTot<<")"<<" ["<<hTot<<"]"<<endl;
+            if(cTot >= catSize){
+                catEdge[cCat] = mva->GetXaxis()->GetBinLowEdge(i+1);
+                catCenter[cCat] = (float)cCenter/(float)cTot;
+                cout<<" -----> cat "<<cCat<<", "<<cTot<<" ["<<catEdge[cCat]<<"]"<<endl;
+                cTot = 0;
+                cCenter = 0;
+                cCat++;
+            }
+            if(i==nBinsMva){
+                catCenter[cCat] = (float)cCenter/(float)cTot;
+                catEdge[cCat] = xMax;
+                lastCat = cCat;
+                cout<<" -----> cat "<<cCat<<", "<<cTot<<" ["<<catEdge[cCat]<<"]"<<endl;
+            }
+        }
+
+        cout<<endl;
+        nCat = lastCat + 1;
+        float totEff = 0;
+        float totW = 0;
+        float avgD = 0;
+        float totP = 0;
+
+        for(int i=0; i<nCat; ++i)
+        {
+            catEff[i] = (float)(catWT[i] + catRT[i]) / (float)nEvents;
+            totEff += catEff[i];
+
+            catW[i] = (float)catWT[i] / (float)(catWT[i] + catRT[i]);
+            avgD += catEff[i]*(1-2*catW[i]);
+
+            catP[i] = catEff[i]*pow(1-2*catW[i],2);
+            totP += catP[i];
+
+
+            vexh[i] = catEdge[i] - catCenter[i];
+            if(i!=0) vexl[i] = catCenter[i] - catEdge[i-1];
+            else     vexl[i] = catCenter[i] - xMin;
+
+            vey[i] = sqrt(((float)catWT[i] * (float)catRT[i])/pow(((float)catWT[i] + (float)catRT[i]),3) );
+
+            cout<<"CAT "<<i+1;
+            if(i!=0) cout<<" ["<<catEdge[i-1]<<" - "<<catEdge[i]<<" ] ";
+            else     cout<<" ["<<xMin<<" - "<<catEdge[i]<<" ] ";
+            cout<<" - eff = "<<100*catEff[i]<<"%, w = "<<100*catW[i];
+            cout<<"%, P = "<<100*catP[i]<<"%"<<endl;
+            //cout<<" - "<<catCenter[i]<<" - ";
+            //cout<<catRT[i] + catWT[i]<<endl;
+        }
+
+        ofstream ofs;
+        ofs.open ("cat.txt");
+        ofs<<nCat<<endl;
+        for(int i=0; i<nCat; ++i)
+        {
+            if(i!=0) ofs<<catEdge[i-1];
+            else     ofs<<xMin;
+            ofs<<" "<<catEdge[i]<<" ";
+            ofs<<catW[i]<<endl;
+        }
+        ofs.close();
+
+        avgD /= totEff;
+        float avgW = (1-avgD)/2;
+
+        cout<<endl<<"Total Eff = "<<100*totEff<<"%"<<endl;
+        cout<<"Averange W = "<<100*avgW<<"%"<<endl;
+        cout<<"Total P = "<<100*totP<<"%"<<endl;
+
+        cout<<endl<<"NoCat Eff = "<<100.*(float)(nRT+nWT)/nEvents<<"%"<<endl;
+        cout<<"NoCat W = "<<100.*(float)nWT/(nRT+nWT)<<"%"<<endl;
+        cout<<"NoCat P = "<<100.*((float)(nRT+nWT)/nEvents)*pow(1.-2.*((float)nWT/(nRT+nWT)),2)<<"%"<<endl;
+
+        TGraph* gr = new TGraphAsymmErrors(nCat,catCenter,catW,vexl,vexh,vey,vey);
+        TCanvas *c1 = new TCanvas();
+        gr->SetMarkerStyle(20);
+        gr->SetMarkerSize(.5);
+        gr->SetTitle("per-event mistag");
+        gr->GetXaxis()->SetTitle("MVA output");
+        gr->Draw("APE");
+
+        TCanvas *c2 = new TCanvas();
+        mva_WT->Draw("hist");
+        mva_RT->Draw("hist same");
+        mva_WT->SetLineColor(kRed);
+
+        TCanvas *c3 = new TCanvas();
+        gr->Draw("APE");
+        fW->SetLineColor(kGreen);
+        fW->DrawClone("P SAME");
     }
 
-    ofstream myfile;
-    myfile.open ("cat.txt");
-    for(int i=0; i<nCat; ++i)
-    {
-        if(i!=0) myfile<<catEdge[i-1];
-        else     myfile<<xMin;
-        myfile<<" "<<catEdge[i]<<" ";
-        myfile<<catW[i]<<endl;
-    }
-    myfile.close();
 
-    avgD /= totEff;
-    float avgW = (1-avgD)/2;
-
-    cout<<endl<<"Total Eff = "<<100*totEff<<"%"<<endl;
-    cout<<"Averange W = "<<100*avgW<<"%"<<endl;
-    cout<<"Total P = "<<100*totP<<"%"<<endl;
-
-    cout<<endl<<"NoCat Eff = "<<100.*(float)(nRT+nWT)/nEvents<<"%"<<endl;
-    cout<<"NoCat W = "<<100.*(float)nWT/(nRT+nWT)<<"%"<<endl;
-    cout<<"NoCat P = "<<100.*((float)(nRT+nWT)/nEvents)*pow(1.-2.*((float)nWT/(nRT+nWT)),2)<<"%"<<endl;
-
-    TGraph* gr = new TGraphAsymmErrors(nCat,catCenter,catW,vexl,vexh,vey,vey);
-    TCanvas *c1 = new TCanvas();
-    gr->SetMarkerStyle(20);
-    gr->SetMarkerSize(.5);
-    gr->SetTitle("per-event mistag");
-    gr->GetXaxis()->SetTitle("MVA output");
-    gr->Draw("APE");
-
-    TCanvas *c2 = new TCanvas();
-    mva_WT->Draw("hist");
-    mva_RT->Draw("hist same");
-    mva_WT->SetLineColor(kRed);
-
-    TCanvas *c3 = new TCanvas();
-    gr->Draw("APE");
-    fW->SetLineColor(kGreen);
-    fW->DrawClone("P SAME");
 }
