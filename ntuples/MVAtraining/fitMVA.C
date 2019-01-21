d65b79409bf2bfca9424e4411ca7924ced025655
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-01-21 17:20:00 +0100

    better fit

--- ntuples/MVAtraining/fitMVA.C
+++ ntuples/MVAtraining/fitMVA.C
@@ -801,22 +801,19 @@ pair<float, float> CountEventsWithFit(TH1 *hist, TString name = "hist"){
     if(process_.Contains("BuJPsiK"))   mean = 5.2793;
     if(process_.Contains("BdJPsiKx"))  mean = 5.2796;
 
-    TString funcDef = "[1]*TMath::Gaus(x, [0], [4], true)";
-    funcDef +=       "+[2]*TMath::Gaus(x, [0], [5], true)";
-    funcDef +=       "+[3]*TMath::Gaus(x, [0], [6], true)";
-    funcDef = "+[7]+[8]*TMath::Erfc([9]*(x-[10]))";
+    TString sgnDef = "[1]*TMath::Gaus(x, [0], [4], true)";
+    sgnDef +=       "+[2]*TMath::Gaus(x, [0], [5], true)";
+    sgnDef +=       "+[3]*TMath::Gaus(x, [0], [6], true)";
+
+    TString bkgDef = "[7]+[8]*TMath::Erfc([9]*(x-[10]))";
+    if(hist->GetEntries()<=250) bkgDef = "[7]";
+
+    TString funcDef = sgnDef + "+" + bkgDef;
 
     TF1 *func = new TF1("func", funcDef, min_, max_);
 
+    //SIGNAL
     float limit = hist->GetEntries()*hist->GetBinWidth(1);
-
-    float bkgHeight = 0;
-    for(int i=0; i<5; i++){
-        bkgHeight += hist->GetBinContent(i+i);
-        bkgHeight += hist->GetBinContent(hist->GetNbinsX()-i);
-    }
-
-    bkgHeight /= 10;
 
     func->SetParameter(0, mean);
     func->SetParameter(1, limit/3);
@@ -825,23 +822,33 @@ pair<float, float> CountEventsWithFit(TH1 *hist, TString name = "hist"){
     func->SetParameter(4, sigma);
     func->SetParameter(5, sigma);
     func->SetParameter(6, sigma);
-
     func->SetParLimits(0, mean-sigma, mean+sigma);
-    func->SetParLimits(1, 0, hist->GetEntries());
-    func->SetParLimits(2, 0, hist->GetEntries());
-    func->SetParLimits(3, 0, hist->GetEntries());
-    func->SetParLimits(4, sigma/2, sigma*2.5);
-    func->SetParLimits(5, sigma/2, sigma*2.5);
-    func->SetParLimits(6, sigma/2, sigma*2.5);
+    func->SetParLimits(1, 0, limit);
+    func->SetParLimits(2, 0, limit);
+    func->SetParLimits(3, 0, limit);
+    func->SetParLimits(4, sigma/2, sigma*2);
+    func->SetParLimits(5, sigma/2, sigma*2);
+    func->SetParLimits(6, sigma/2, sigma*2);
 
+
+    //BKG
+
+    float bkgHeight = 0.;
+    int nBinsBkgEst = 7;
+    for(int i=0; i<nBinsBkgEst; i++)
+        bkgHeight += hist->GetBinContent(hist->GetNbinsX()-i);
+    bkgHeight /= nBinsBkgEst;
 
     func->SetParameter(7, bkgHeight);
-    func->SetParameter(8, 1);
-    func->SetParameter(9, 20);
-    func->SetParameter(10, 5.10);
-    func->SetParLimits(8, 0, bkgHeight);
-    func->SetParLimits(9, 10, 1e3);
-    func->SetParLimits(10, 5.0, mean);
+    if(hist->GetEntries()>250){
+        func->SetParameter(8, hist->GetBinContent(1)/2);
+        func->SetParameter(9, 10);
+        func->SetParameter(10, 5);
+        func->SetParLimits(7, 0, bkgHeight*2);
+        func->SetParLimits(8, 0, hist->GetBinContent(1));
+        //func->SetParLimits(9, 10, 1e3);
+        //func->SetParLimits(10, 5.0, mean);
+    }
 
     func->SetNpx(2000);
 
@@ -850,37 +857,34 @@ pair<float, float> CountEventsWithFit(TH1 *hist, TString name = "hist"){
     hist->SetMarkerSize(.75);
     TFitResultPtr r = hist->Fit("func","MRLS");
     hist->Draw("PE");
-    hist->SetMinimum(0.1);
+    hist->SetMinimum(0);
 
     TF1 *fit = hist->GetFunction("func");
     TF1 *f1 = new TF1("f1","[0]*TMath::Gaus(x, [1], [2], true)", min_, max_);
     TF1 *f2 = new TF1("f2","[0]*TMath::Gaus(x, [1], [2], true)", min_, max_);
     TF1 *f3 = new TF1("f3","[0]*TMath::Gaus(x, [1], [2], true)", min_, max_);
-    TF1 *f4 = new TF1("f4","[0]", min_, max_);;
-    TF1 *f5 = new TF1("f5","[0]*TMath::Erfc([1]*(x-[2]))", min_, max_);;
+    TF1 *f4 = new TF1("f4","[0]+[1]*TMath::Erfc([2]*(x-[3]))", min_, max_);
+    if(hist->GetEntries()<=250) f4 = new TF1("f4","[0]", min_, max_);
 
     f1->SetParameters(fit->GetParameter(1),fit->GetParameter(0),fit->GetParameter(4));
     f2->SetParameters(fit->GetParameter(2),fit->GetParameter(0),fit->GetParameter(5));
     f3->SetParameters(fit->GetParameter(3),fit->GetParameter(0),fit->GetParameter(6));
-    f4->SetParameter(0, fit->GetParameter(7));
-    f5->SetParameters(fit->GetParameter(8),fit->GetParameter(9),fit->GetParameter(10));
+    if(hist->GetEntries()<=250) f4->SetParameter(0,fit->GetParameter(7));
+    else f4->SetParameters(fit->GetParameter(7),fit->GetParameter(8),fit->GetParameter(9),fit->GetParameter(10));
 
     f1->SetLineColor(kBlue);
     f2->SetLineColor(kViolet);
     f3->SetLineColor(kAzure);
-    f4->SetLineColor(kOrange); 
-    f5->SetLineColor(kGreen);
+    f4->SetLineColor(kOrange);
     f1->SetLineStyle(2);
     f2->SetLineStyle(2);
     f3->SetLineStyle(2);
-    f4->SetLineStyle(2); 
-    f5->SetLineStyle(2);
+    f4->SetLineStyle(2);
 
     f1->Draw("same");
     f2->Draw("same");
     f3->Draw("same");
-    f4->Draw("same"); 
-    f5->Draw("same");
+    f4->Draw("same");
     c5->Print(name + ".pdf");
 
     float nEvt = fit->GetParameter(1);
@@ -889,7 +893,6 @@ pair<float, float> CountEventsWithFit(TH1 *hist, TString name = "hist"){
     nEvt/=hist->GetBinWidth(1);
 
     TMatrixDSym cov = r->GetCovarianceMatrix();
-
     float errN = sqrt(cov(1,1)+cov(2,2)+cov(3,3)+2*(cov(1,2)+cov(1,3)+cov(2,3)))/hist->GetBinWidth(1);
 
     return make_pair(nEvt, errN);
