0467894e624bade442b7d2b7f1c84bb5bc636417
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-02-27 15:57:16 +0100

    script updated

--- ntuples/BuData/bsub17D.sh
+++ ntuples/BuData/bsub17D.sh
@@ -9,7 +9,7 @@ while [ "$i" -le "$max" ]; do
   echo $'#!/bin/sh' > script.sh
   echo $'#BSUB -o test.log' >> script.sh
   echo $'eval `scram runtime -sh`' >> script.sh
-  echo "pdTreeAnalyze /lustre/cmswork/abragagn/ntuList/charmonium2017Lists/Charmonium_Run2017D-31Mar2018-v1_MINIAOD_DCAP.list hist$i.root -v outputFile ntu$i.root -v histoMode RECREATE -v use_gen f -v muonIdWp 0.4 -v useHLT t -v process BuJPsiK -n $n -s $skip" >> script.sh
+  echo "pdTreeAnalyze /lustre/cmswork/abragagn/ntuList/charmonium2017Lists/Charmonium_Run2017D-31Mar2018-v1_MINIAOD_DCAP.list hist$i.root -v outputFile ntu$i.root -v histoMode RECREATE -v use_gen f -v useHLT t -v process BuJPsiK -n $n -s $skip" >> script.sh
   echo "" >> script.sh
   bsub < script.sh;
   cd ..;
