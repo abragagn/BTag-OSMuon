63a6a58e24d1b36d50e47d42837eae7641bbaf7f
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-03-07 16:09:50 +0100

    skimFiles.C

---
+++ ntuples/skimFiles.C
@@ -0,0 +1,10 @@
+void skimFiles(TString filename = "", Tstring cut = "")
+{
+	TFile *of = new TFile(filename);
+	TTree *ot = (TTree*)of->Get("PDsecondTree");
+	TString filename_( filename(0, filename.Index(".root")) )
+	TFile *nf = new TFile(filename_ + "_skim.root", "RECREATE");
+	TTree *nt = ot->CopyTree(cut);
+	nt->Write();
+	nf->Close();
+}
