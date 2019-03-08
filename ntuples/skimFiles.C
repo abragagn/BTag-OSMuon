15ec1c546b17000b53fde7446dc9d753dbe6b02a
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-03-08 09:50:43 +0100

    Typos

--- ntuples/skimFiles.C
+++ ntuples/skimFiles.C
@@ -1,8 +1,8 @@
-void skimFiles(TString filename = "", Tstring cut = "")
+void skimFiles(TString filename = "", TString cut = "")
 {
 	TFile *of = new TFile(filename);
 	TTree *ot = (TTree*)of->Get("PDsecondTree");
-	TString filename_( filename(0, filename.Index(".root")) )
+	TString filename_( filename(0, filename.Index(".root")) );
 	TFile *nf = new TFile(filename_ + "_skim.root", "RECREATE");
 	TTree *nt = ot->CopyTree(cut);
 	nt->Write();
