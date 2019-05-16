0c41bf75c416b28937bae14bf2977aebc4715e23
AlbertoBragagnolo <alberto.bragagnolo@pd.infn.it>
2019-05-16 11:52:39 +0200

    formatting

--- ntuples/skimFiles.C
+++ ntuples/skimFiles.C
@@ -1,10 +1,10 @@
 void skimFiles(TString filename = "", TString cut = "")
 {
-	TFile *of = new TFile(filename);
-	TTree *ot = (TTree*)of->Get("PDsecondTree");
-	TString filename_( filename(0, filename.Index(".root")) );
-	TFile *nf = new TFile(filename_ + "_skim.root", "RECREATE");
-	TTree *nt = ot->CopyTree(cut);
-	nt->Write();
-	nf->Close();
+    TFile *of = new TFile(filename);
+    TTree *ot = (TTree*)of->Get("PDsecondTree");
+    TString filename_( filename(0, filename.Index(".root")) );
+    TFile *nf = new TFile(filename_ + "_skim.root", "RECREATE");
+    TTree *nt = ot->CopyTree(cut);
+    nt->Write();
+    nf->Close();
 }
